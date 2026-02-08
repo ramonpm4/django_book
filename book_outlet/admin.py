5aa420bea5e563b1c13f2c65487efcaa13a9e597
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-08 19:56:53 -0300

    prepopulated_fields in admin.py

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from .models import Book
 # Register your models here.
 
-admin.site.register(Book) # Le dice que quiero administrar este model a traves de la admin interface.
+class BookAdmin(admin.ModelAdmin): # Configura administration for the Book model.
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Book, BookAdmin) # Le dice que quiero administrar este model a traves de la admin interface.
