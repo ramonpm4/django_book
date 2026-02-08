3ff798eb2d77a912ba49ac36d5d95a3374bdbe80
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-08 15:38:22 -0300

    book_detail view + urls.py

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("<int:id>", views.book_detail) # digo id porque en la view function yo defini que recibiría id.
 ]
 
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Book
 
@@ -9,4 +9,19 @@ def index(request) -> HttpResponse:
     books = Book.objects.all()
     return render(request, 'book_outlet/index.html', {
         'books': books
+    })
+    
+
+def book_detail(request, id) -> HttpResponse:
+    # try: 
+    #     book = Book.objects.get(pk=id) # pk: primary key
+    # except:
+    #     raise Http404()
+    
+    book = get_object_or_404(Book, pk=id) # Es lo mismo que lo de arriba.
+    return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestseller': book.is_best_selling
     })
