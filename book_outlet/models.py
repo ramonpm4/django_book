a23097ef34849b7640af77fc2714a99afcd0d8a7
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-08 16:35:20 -0300

    save method overwrite for slug

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 
 class Book(models.Model):
@@ -8,11 +9,14 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_best_selling = models.BooleanField(default=False)
-    
+    slug = models.SlugField(default="", null=False) # NEW
     
     def get_absolute_url(self) -> str:
         return reverse('book-detail', kwargs={"pk": self.pk})
     
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title) # Esto crea un slug a partir del title antes de que se guarde (abajo).
+        super().save( *args, **kwargs) # Esto es para mantener los save methods que ya tenemos. 
     
     def __str__(self) -> str:
         return f"{self.title} ({self.rating})"
