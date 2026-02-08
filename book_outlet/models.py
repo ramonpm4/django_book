55f0d2351ab54513e21132ba0646c5ef7a8c258f
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-08 16:02:03 -0300

    get_absolute_url overwrite

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 # Create your models here.
 
 class Book(models.Model):
@@ -7,6 +8,11 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_best_selling = models.BooleanField(default=False)
+    
+    
+    def get_absolute_url(self) -> str:
+        return reverse('book-detail', kwargs={"pk": self.pk})
+    
     
     def __str__(self) -> str:
         return f"{self.title} ({self.rating})"
