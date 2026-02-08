d94c8f8ffe8eb2a8bbc8f9c4e9e1514f63a55c5a
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-08 19:43:58 -0300

    blank=True en slug

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Book
 # Register your models here.
+
+admin.site.register(Book) # Le dice que quiero administrar este model a traves de la admin interface.
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_best_selling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True) # NEW
+    slug = models.SlugField(default="", blank=True,  null=False, db_index=True) # NEW
     
     def get_absolute_url(self) -> str:
         return reverse('book-detail', args=[self.slug])
