f4a461133383fbf970fbf1e47e8e523aac8db86f
Ramón <ramonperaltamartinez@Ramons-MacBook-Air.local>
2026-02-10 16:44:41 -0300

    null=True e n adress

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,9 +4,17 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+
+class Adress(models.Model):
+    street = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=6)
+    city = models.CharField(max_length=50)
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    adress = models.OneToOneField(Adress, on_delete=models.CASCADE, null=True)
     
     def full_name(self) -> str:
         return f"{self.first_name} {self.last_name}"
